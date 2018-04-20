5ba7f7e5a85448e6b6117d69097a80d17bf762b8
Yvonna Leung <yvonna.leung@gmail.com>
2018-04-19 22:27:49 -0700

    Submitting homework
    Implemented makeCacheMatrix and cacheSolve
    
    Tested by showing that the inverse matrix is actually correct by multiplying it by the original matrix and comparing it to the identity matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        #if the inverse has already been calculated
+        if(!is.null(inv)) {
+                #get it from the cache
+                message("getting cached data")
+                return(inv)
+        }
+        #if the inverse has not been calculated, calculate the inverse
+        data <- x$get()
+        inv <- solve(data, ...)
+        #set the value of the inverse in the cache via the setinv function
+        x$setInverse(inv)
+        inv
 }
